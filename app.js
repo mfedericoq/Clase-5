2aa63af5729cb5d5632446b825f4a9faf484e3f1
mfedericoq <martinfquiroga@gmail.com>
2023-03-16 22:45:58 -0300

    Revision final trabajo clase 5

---
+++ app.js
@@ -0,0 +1,37 @@
+class Input {
+    constructor(parentID, text) {
+          this.parentID = parentID;
+          this.text = text;
+    }
+    render() {
+        let myInput = document.getElementById(this.parentID);
+        const myInputHTML = myInput.innerHTML + "<input type='" + this.text + "'>";
+        myInput.innerHTML = myInputHTML;
+    }
+}
+
+const inputtypes = ["checkbox","text","date","password","radio"];
+
+//asignacion tipo de input
+/*primer método (corroborar con profe si es viable o poco práctico)
+const valorInput = inputtypes.forEach(createInput);
+*/
+
+const inputtypesfiltered = inputtypes.filter(element => element !="radio");
+
+inputtypesfiltered.forEach(element => createInput(element));
+
+function createInput(text) {   
+    let tipoinput = new Input("input1", text);
+    tipoinput.render();
+}
+
+//segundo método
+//inputtypes.forEach(element => createInput(element));
+
+
+
+//nueva variable con push
+/*inputtypes.push("submit");
+createInput("submit");*/
+
